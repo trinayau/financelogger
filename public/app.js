6ff1f3d11f37a9f3f2da393926ec68233e5dd5a5
Trina <92634994+trinayau@users.noreply.github.com>
2022-06-20 23:04:13 +0100

    added formatter

--- public/app.js
+++ public/app.js
@@ -1,14 +1,19 @@
-"use strict";
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
+const me = {
+    name: 'Trina',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log(' I spent' + amount);
+        return amount;
     }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+};
+const greetPerson = (person) => {
+    console.log('Hello, ' + person.name);
+};
+greetPerson(me);
+import { Invoice } from './models/invoice.js';
 const invOne = new Invoice('mario', 'work on mario website', 250);
 const invTwo = new Invoice('luigi', 'work on luigi website', 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
-class Invoice {
+//interfaces
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
 
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number,
-    ) {
-    }
-
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
+const me: IsPerson = {
+    name: 'Trina',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+         console.log(' I spent' + amount);
+         return amount;
     }
 }
+
+const greetPerson = (person: IsPerson): void => {
+    console.log('Hello, ' + person.name);
+}
+
+greetPerson(me);
+import { Invoice } from './models/invoice.js';
 
 const invOne = new Invoice('mario', 'work on mario website', 250);
 const invTwo = new Invoice('luigi', 'work on luigi website', 300);
