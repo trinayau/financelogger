5042efce1ea08f3674ca02763c4092efe3c1ca99
Trina <92634994+trinayau@users.noreply.github.com>
2022-06-26 14:22:24 +0100

    remove commented code and add list render method

--- public/app.js
+++ public/app.js
@@ -1,30 +1,15 @@
+import { ListTemplate } from './models/ListTemplate.js';
 import { Payment } from './models/payment.js';
 import { Invoice } from './models/invoice.js';
-// This var must follow the interface HasFormatter
-// Making sure that whatever object is stored inside docOne variable has this interface
-// Structures code and makes things more secure
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'work on the mario website', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-// // restricting array of objects that only implement HasFormatter interface
-// let docs: HasFormatter[]= [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice('mario', 'work on mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on luigi website', 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// console.log(invoices);
 const form = document.querySelector('.new-item-form');
-// console.log(form.children)
 //inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -34,5 +19,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,19 @@
+import { ListTemplate } from './models/ListTemplate.js';
 import { Payment } from './models/payment.js';
 import { Invoice } from './models/invoice.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
-// This var must follow the interface HasFormatter
-// Making sure that whatever object is stored inside docOne variable has this interface
-// Structures code and makes things more secure
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'work on the mario website', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-
-// // restricting array of objects that only implement HasFormatter interface
-// let docs: HasFormatter[]= [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-
-// const invOne = new Invoice('mario', 'work on mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on luigi website', 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// console.log(invoices);
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children)
-
 //inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
@@ -45,5 +25,6 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
 
-    console.log(doc);
+    list.render(doc, type.value, 'end');
+
 })
